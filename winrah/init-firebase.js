5038683a38dcf04f4bb1f877cdeaab4b728cc89d
mahmoudbens <mahmoudbensfia@gmail.com>
2020-11-20 10:43:52 +0100

    winrah edits  rest clustering

---
+++ winrah/init-firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCRgKqqj1pvvI_1jdg3f-me_K5PdBQ7eDk",
+    authDomain: "win-rah.firebaseapp.com",
+    databaseURL: "https://win-rah.firebaseio.com",
+    projectId: "win-rah",
+    storageBucket: "win-rah.appspot.com",
+    messagingSenderId: "146453456136",
+    appId: "1:146453456136:web:fd91311157f3c241b038bd",
+    measurementId: "G-MNLGDTQM4L"
+  };
+  
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
